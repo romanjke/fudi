8e85cec6c9b714babee49091b5fa1d4d0f61e0c7
Roman <romanjkez0r@gmail.com>
2018-08-13 09:05:52 +0300

    add app, testimonials

--- src/blocks/main.js
+++ src/blocks/main.js
@@ -1,13 +1,17 @@
-/*******************************************************
-                    MODULE bigSlide
-*******************************************************/
-
 $(document).ready(function() {
+    //bigSlide
     $('.header__nav-toggle').bigSlide({
         side: 'right',
         speed: 600,
         menuWidth: '320px'
     });
 
+    //onePageNav
     $('#menu').onePageNav();
+
+    //flexslider
+    $('.flexslider').flexslider({
+        animation: "slide",
+        directionNav: false
+    });
 });
